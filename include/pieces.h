9b905500cee6cc792e8ea75d56b710282c6193a1
Max <maxruigrok190@gmail.com>
2023-07-04 16:54:35 +0200

    No chess yet but 0 errors and 0 warnings yippie

--- include/pieces.h
+++ include/pieces.h
@@ -9,7 +9,6 @@
 class Piece {
 public:
     Piece(bool nwIsWhite);
-    virtual ~Piece();
     
     virtual std::vector<Move> getLegalMoves() = 0;
 
@@ -19,37 +18,37 @@ public:
 
 class Pawn: public Piece {
 public:
-
+    Pawn();
     std::vector<Move> getLegalMoves();
 };
 
 class Knight: public Piece {
 public:
-
+    Knight();
     std::vector<Move> getLegalMoves();
 };
 
 class Bishop: public Piece {
 public:
-
+    Bishop();
     std::vector<Move> getLegalMoves();
 };
 
 class Rook: public Piece {
 public:
-
+    Rook();
     std::vector<Move> getLegalMoves();
 };
 
 class Queen: public Piece {
 public:
-
+    Queen();
     std::vector<Move> getLegalMoves();
 };
 
 class King: public Piece {
 public:
-
+    King();
     std::vector<Move> getLegalMoves();
 };
 
--- src/pieces.cc
+++ src/pieces.cc
@@ -5,26 +5,38 @@ Piece::Piece(bool nwIsWhite) {
 }
 
 std::vector<Move> Pawn::getLegalMoves() {
-
+    std::vector<Move> moves;
+    
+    return moves;
 }
 
 std::vector<Move> Knight::getLegalMoves() {
+    std::vector<Move> moves;
     
+    return moves;
 }
 
 std::vector<Move> Bishop::getLegalMoves() {
+    std::vector<Move> moves;
     
+    return moves;
 }
 
 std::vector<Move> Rook::getLegalMoves() {
+    std::vector<Move> moves;
     
+    return moves;
 }
 
 std::vector<Move> Queen::getLegalMoves() {
+    std::vector<Move> moves;
     
+    return moves;
 }
 
 std::vector<Move> King::getLegalMoves() {
+    std::vector<Move> moves;
     
+    return moves;
 }
 
